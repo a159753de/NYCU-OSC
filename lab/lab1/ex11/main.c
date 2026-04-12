07e43f9c12df86402d6228ab882f5b6dc960ee2b
a159753de <g6961116@gmail.com>
2026-04-12 14:32:53 +0800

    add lab1

---
+++ lab/lab1/ex11/main.c
@@ -0,0 +1,60 @@
+#define UART_BASE 0x10000000UL
+#define UART_RBR  (unsigned char*)(UART_BASE + 0x0) // Receiver Holding Register (讀取接收到的字元)
+#define UART_THR  (unsigned char*)(UART_BASE + 0x0) // Transmitter Holding Register (寫入要傳送的字元)
+#define UART_LSR  (unsigned char*)(UART_BASE + 0x5) // Line Status Register (檢查是否準備好讀/寫,以及其他狀態)
+#define LSR_DR    (1 << 0) // Data Ready - 接收緩衝區有資料可讀
+#define LSR_TDRQ  (1 << 5) // Transmitter Data Request - 發送緩衝區空，可以寫入新資料
+
+// string buffer
+char * str_buf;
+
+// if equal, return 0; if s1 > s2, return positive; if s1 < s2, return negative
+int strcmp(const char * s1, const char * s2){
+    while(*s1 && (*s1 == *s2)){
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2; 
+}
+
+char uart_getc() {
+    // TODO: Implement this function
+    while (!(*UART_LSR & LSR_DR)); //the 2 value is doing bitwise AND, if the result is 0, it means the data is not ready, so we keep waiting. If it's not 0, it means data is ready, and we can read it.
+    return *UART_RBR;
+}
+
+void uart_putc(char c) {
+    // TODO: Implement this function
+    while (!(*UART_LSR & LSR_TDRQ));
+    *UART_THR = c;
+    *str_buf = c;
+    str_buf++;
+    if (c == '\r'){
+        *str_buf = '\0';
+        uart_putc('\n');
+        uart_puts(str_buf); // print the string buffer
+        uart_puts("opi-rv2> ");
+    }
+}
+
+void uart_puts(const char* s) {
+    // TODO: Implement this function
+    if(strcmp(s, "hello\0") == 0){
+        char* hello = "Hello World";
+        while (*hello) {
+            uart_putc(*hello++);
+        }
+    }else{
+        while (*s) {
+        uart_putc(*s++);
+        }    
+    }
+}
+
+void start_kernel() {
+    uart_puts("\nStarting kernel ...\n");
+    uart_puts("opi-rv2> ");
+    while (1) {
+        uart_putc(uart_getc());
+    }
+}
---
+++ lab/lab1/main.c
@@ -0,0 +1,137 @@
+extern char uart_getc(void);
+extern void uart_putc(char c);
+extern void uart_puts(const char* s);
+extern void uart_hex(unsigned long h);
+
+#define SBI_EXT_BASE      0x10
+
+enum sbi_ext_base_fid {
+    SBI_EXT_BASE_GET_SPEC_VERSION,
+    SBI_EXT_BASE_GET_IMP_ID,
+    SBI_EXT_BASE_GET_IMP_VERSION,
+    SBI_EXT_BASE_PROBE_EXT,
+    SBI_EXT_BASE_GET_MVENDORID,
+    SBI_EXT_BASE_GET_MARCHID,
+    SBI_EXT_BASE_GET_MIMPID,
+};
+
+struct sbiret {
+    long error;
+    long value;
+};
+
+struct sbiret sbi_ecall(int ext,
+                        int fid,
+                        unsigned long arg0,
+                        unsigned long arg1,
+                        unsigned long arg2,
+                        unsigned long arg3,
+                        unsigned long arg4,
+                        unsigned long arg5) {
+    struct sbiret ret;
+    register unsigned long a0 asm("a0") = (unsigned long)arg0;
+    register unsigned long a1 asm("a1") = (unsigned long)arg1;
+    register unsigned long a2 asm("a2") = (unsigned long)arg2;
+    register unsigned long a3 asm("a3") = (unsigned long)arg3;
+    register unsigned long a4 asm("a4") = (unsigned long)arg4;
+    register unsigned long a5 asm("a5") = (unsigned long)arg5;
+    register unsigned long a6 asm("a6") = (unsigned long)fid;
+    register unsigned long a7 asm("a7") = (unsigned long)ext;
+    asm volatile("ecall"
+                 : "+r"(a0), "+r"(a1)
+                 : "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+                 : "memory");
+    ret.error = a0;
+    ret.value = a1;
+    return ret;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+/**
+ * sbi_get_spec_version() - Get the SBI specification version.
+ *
+ * Return: The current SBI specification version.
+ * The minor number of the SBI specification is encoded in the low 24 bits,
+ * with the major number encoded in the next 7 bits. Bit 31 must be 0.
+ */
+long sbi_get_spec_version(void) {
+    // TODO: Implement this function
+    return sbi_ecall(SBI_EXT_BASE, 0x0, 0, 0, 0, 0, 0, 0).value;
+}
+
+long sbi_get_impl_id(void) {
+    // TODO: Implement this function
+    return sbi_ecall(SBI_EXT_BASE, 0x1, 0, 0, 0, 0, 0, 0).value;
+}
+
+long sbi_get_impl_version(void) {
+    // TODO: Implement this function
+    return sbi_ecall(SBI_EXT_BASE, 0x2, 0, 0, 0, 0, 0, 0).value;
+}
+
+/**
+ * sbi_probe_extension() - Check if an SBI extension ID is supported or not.
+ * @extid: The extension ID to be probed.
+ *
+ * Return: 1 or an extension specific nonzero value if yes, 0 otherwise.
+ */
+long sbi_probe_extension(int extid) {
+    // TODO: Implement this function
+    return sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_PROBE_EXT, extid, 0, 0, 0, 0, 0).value;
+    
+}
+
+void start_kernel() {
+    uart_puts("Starting kernel ...\n");
+    uart_puts("opi-rv2> ");
+
+    char str_buf[1024];
+    str_buf[0] = '\0';
+    int index = 0;
+
+    while (1) {
+        char c = uart_getc();
+        uart_putc(c);
+        if (c == '\r' || c == '\n') {
+            str_buf[index] = '\0';
+
+            if(strcmp(str_buf, "hello\0") == 0){
+                uart_puts("Hello World.\n");
+            }else if(strcmp(str_buf, "help\0") == 0){
+                uart_puts("Available commands:\r\n  help  - Show all commands\r\n  hello - Print Hello World\r\n  info  - Print system information\r\n");
+            }else if(strcmp(str_buf, "info\0") == 0){
+                uart_puts("System Information:\n");
+                uart_puts("  OpenSBI specification version: ");
+                uart_hex(sbi_get_spec_version());
+                uart_puts("\n");
+
+                uart_puts("  Implementation ID: ");
+                uart_hex(sbi_get_impl_id());
+                uart_puts("\n");
+
+                uart_puts("  Implementation version: ");
+                uart_hex(sbi_get_impl_version());
+                uart_puts("\n");
+            }else{
+                char* unknown = "Unknown command.\n";
+                while (*unknown) {
+                    uart_putc(*unknown++);
+                }
+                uart_puts("Use \"help\" to get commands.\n");
+            }
+            
+            uart_puts("opi-rv2> ");
+            index = 0; // reset the index for the next input
+        } else {
+            str_buf[index] = c; // append the character to the string buffer
+            index++;
+        }
+    }
+}
