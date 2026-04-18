d6c850926cd2f4ea4ff86f85aa2f6d0fddf47391
a159753de <g6961116@gmail.com>
2026-04-19 00:07:00 +0800

    done ex11

--- exercise/ex11/main.c
+++ exercise/ex11/main.c
@@ -1,7 +1,7 @@
-#define UART_BASE 0x10000000UL
+#define UART_BASE 0xD4017000
 #define UART_RBR  (unsigned char*)(UART_BASE + 0x0) // Receiver Holding Register (讀取接收到的字元)
 #define UART_THR  (unsigned char*)(UART_BASE + 0x0) // Transmitter Holding Register (寫入要傳送的字元)
-#define UART_LSR  (unsigned char*)(UART_BASE + 0x5) // Line Status Register (檢查是否準備好讀/寫,以及其他狀態)
+#define UART_LSR  (unsigned char*)(UART_BASE + 0x14) // Line Status Register (檢查是否準備好讀/寫,以及其他狀態)
 #define LSR_DR    (1 << 0) // Data Ready - 接收緩衝區有資料可讀
 #define LSR_TDRQ  (1 << 5) // Transmitter Data Request - 發送緩衝區空，可以寫入新資料
 
@@ -15,15 +15,13 @@ void uart_putc(char c) {
     // TODO: Implement this function
     while (!(*UART_LSR & LSR_TDRQ));
     *UART_THR = c;
-    if (c == '\r'){
-        uart_putc('\n');
-        uart_puts("opi-rv2> ");
-    }
 }
 
 void uart_puts(const char* s) {
     // TODO: Implement this function
     while (*s) {
+        // 如果遇到 \n，自動補一個 \r
+        if (*s == '\n') uart_putc('\r');
         uart_putc(*s++);
     } 
 }
@@ -32,6 +30,14 @@ void start_kernel() {
     uart_puts("\nStarting kernel ...\n");
     uart_puts("opi-rv2> ");
     while (1) {
-        uart_putc(uart_getc());
+        // uart_putc(uart_getc());
+        char c = uart_getc(); // 等待輸入
+        
+        // 把收到的字印回去，讓使用者看到自己打了什麼
+        if (c == '\r' || c == '\n') {
+            uart_puts("\nopi-rv2> ");
+        } else {
+            uart_putc(c);
+        }
     }
 }
